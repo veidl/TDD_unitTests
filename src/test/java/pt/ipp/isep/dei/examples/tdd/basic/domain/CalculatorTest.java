87bb6a7d3406b993839d0156d34872be9b8ae767
veidl <thomas.polzer@protonmail.com>
2022-05-13 12:25:18 +0200

    refine last changes

--- src/test/java/pt/ipp/isep/dei/examples/tdd/basic/domain/CalculatorTest.java
+++ src/test/java/pt/ipp/isep/dei/examples/tdd/basic/domain/CalculatorTest.java
@@ -116,9 +116,9 @@ public class CalculatorTest {
 
     @ParameterizedTest
     @CsvSource({"2000000000,200000000", "1000000000,2000000000", "-1000000000,-2000000000"})
-    public void ensureAdditionOfTwoBigNumbersCauseUnsupportedException(int a, int b) {
-        UnsupportedOperationException unsupportedOperationException = Assertions.assertThrows(UnsupportedOperationException.class, () -> calculator.sum(a, b));
-        Assertions.assertEquals("Result too big for int", unsupportedOperationException.getMessage());
+    public void ensureAdditionOfTwoBigNumbersCauseUnsupportedOperationException(int a, int b) {
+        UnsupportedOperationException exception = Assertions.assertThrows(UnsupportedOperationException.class, () -> calculator.sum(a, b));
+        Assertions.assertEquals("Result too big for int", exception.getMessage());
     }
 
     @ParameterizedTest
@@ -130,27 +130,27 @@ public class CalculatorTest {
 
     @ParameterizedTest
     @CsvSource({"-2000000000,200000000", "-1000000000,2000000000", "1000000000,-2000000000"})
-    public void ensureSubtractionOfTwoBigNumbersCauseUnsupportedException(int a, int b) {
-        UnsupportedOperationException unsupportedOperationException = Assertions.assertThrows(UnsupportedOperationException.class, () -> calculator.subtract(a, b));
-        Assertions.assertEquals("Result too big for int", unsupportedOperationException.getMessage());
+    public void ensureSubtractionOfTwoBigNumbersCauseUnsupportedOperationException(int a, int b) {
+        UnsupportedOperationException exception = Assertions.assertThrows(UnsupportedOperationException.class, () -> calculator.subtract(a, b));
+        Assertions.assertEquals("Result too big for int", exception.getMessage());
     }
 
     @ParameterizedTest
-    @CsvSource({"2,2,1", "6,3,2", "17039,88,193", "0,1,0", "4,24,0", "0,1,0", "-10,5,-2", "-1347544,42,-32085"})
+    @CsvSource({"2,2,1", "6,3,2", "17039,88,193", "0,1,0", "4,24,0", "34684565,1,34684565", "-10,5,-2", "-1347544,42,-32085"})
     public void ensureDivisionIsCalculatedCorrectly(int a, int b, int expectedResult) {
         int actualResult = calculator.divide(a, b);
         Assertions.assertEquals(expectedResult, actualResult);
     }
 
     @Test
-    public void ensureDivisionThrowsUnsupportedExceptionDivisorIsZero() {
-        UnsupportedOperationException unsupportedOperationException = Assertions.assertThrows(UnsupportedOperationException.class, () -> calculator.divide(11, 0));
-        Assertions.assertEquals("Divisor is zero", unsupportedOperationException.getMessage());
+    public void ensureDivisionThrowsUnsupportedOperationExceptionWhenDivisorIsZero() {
+        UnsupportedOperationException exception = Assertions.assertThrows(UnsupportedOperationException.class, () -> calculator.divide(11, 0));
+        Assertions.assertEquals("Divisor is zero", exception.getMessage());
 
     }
 
     @ParameterizedTest
-    @CsvSource({"2,2,4", "3,6,18", "10,10,100", "0,1,0", "4,24,96", "7,5040,35280", "2,-2,-4", "-100,50,-5000", "0,0,0"})
+    @CsvSource({"2,2,4", "3,6,18", "10,10,100", "0,1,0", "4,24,96", "7,5040,35280", "2,-2,-4", "-100,50,-5000", "0,0,0", "564,0,0"})
     public void ensureMultiplicationIsCalculatedCorrectly(int a, int b, int expectedResult) {
         int actualResult = calculator.multiply(a, b);
         Assertions.assertEquals(expectedResult, actualResult);
@@ -158,19 +158,20 @@ public class CalculatorTest {
 
     @ParameterizedTest
     @CsvSource({"2000000000,200000000", "-300000000,6000000", "-1000000000,-200000000"})
-    public void ensureMultiplicationOfTwoBigNumbersCauseUnsupportedException(int a, int b) {
-        UnsupportedOperationException unsupportedOperationException = Assertions.assertThrows(UnsupportedOperationException.class, () -> calculator.multiply(a, b));
-        Assertions.assertEquals("Result too big for int", unsupportedOperationException.getMessage());
-    }
-
-    @Test
-    public void ensureFactorialOfNegativeNumberThrowsUnsupportedOperationException() {
-        UnsupportedOperationException unsupportedOperationException = Assertions.assertThrows(UnsupportedOperationException.class, () -> calculator.factorial(-1));
-        Assertions.assertEquals("Operand cannot be lt zero", unsupportedOperationException.getMessage());
+    public void ensureMultiplicationOfTwoBigNumbersCauseUnsupportedOperationException(int a, int b) {
+        UnsupportedOperationException exception = Assertions.assertThrows(UnsupportedOperationException.class, () -> calculator.multiply(a, b));
+        Assertions.assertEquals("Result too big for int", exception.getMessage());
     }
 
     @ParameterizedTest
-    @CsvSource({"2,2", "3,6", "10,3628800", "0,1", "4,24", "7,5040"})
+    @ValueSource(ints = {-100, -1000, -66, -33, -21345678})
+    public void ensureFactorialOfNegativeNumberThrowsUnsupportedOperationException(int input) {
+        UnsupportedOperationException exception = Assertions.assertThrows(UnsupportedOperationException.class, () -> calculator.factorial(input));
+        Assertions.assertEquals("Operand cannot be lt zero", exception.getMessage());
+    }
+
+    @ParameterizedTest
+    @CsvSource({"2,2", "3,6", "10,3628800", "0,1", "4,24", "7,5040", "12,479001600"})
     public void ensureFactorialIsCalculatedCorrectly(int input, int expectedResult) {
         int actualResult = calculator.factorial(input);
         Assertions.assertEquals(expectedResult, actualResult);
@@ -178,9 +179,9 @@ public class CalculatorTest {
 
     @ParameterizedTest
     @ValueSource(ints = {100, 1000, 66, 33, 21345678})
-    public void ensureFactorialThrowsErrorForBiggerNumbers(int input) {
-        UnsupportedOperationException unsupportedOperationException = Assertions.assertThrows(UnsupportedOperationException.class, () -> calculator.factorial(input));
-        Assertions.assertEquals("Operand too big for int", unsupportedOperationException.getMessage());
+    public void ensureFactorialThrowsUnsupportedOperationExceptionForBigFactor(int input) {
+        UnsupportedOperationException exception = Assertions.assertThrows(UnsupportedOperationException.class, () -> calculator.factorial(input));
+        Assertions.assertEquals("Operand too big for int", exception.getMessage());
     }
 
 }
